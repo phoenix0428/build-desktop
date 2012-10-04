2b0de4369f0b53e4c813a306b60395ee72e31b6f
Kinam Kim <phoenix0428@gmail.com>
2012-10-04 17:14:07 +0900

    Update scripts/build_nodejs-module-webos-pmlog.sh
    Modified the wrong spell.

--- scripts/build_nodejs-module-webos-pmlog.sh
+++ scripts/build_nodejs-module-webos-pmlog.sh
@@ -15,6 +15,6 @@ $CMAKE -D WEBOS_INSTALL_ROOT:PATH=${LUNA_STAGING} -DCMAKE_INSTALL_PREFIX=${LUNA_
 make -j$PROCCOUNT VERBOSE=1
 make install
 # NOTE: Install built node module to /usr/lib/nodejs. Names have changed; may need to be fixed.
-cp -f webos-plmog.node $ROOTFS/usr/palm/nodejs/
+cp -f webos-pmlog.node $ROOTFS/usr/palm/nodejs/
 # copy old node module names (as symlinks) from staging to ROOTFS
 cp -fd ${LUNA_STAGING}/usr/lib/nodejs/*.node $ROOTFS/usr/palm/nodejs
